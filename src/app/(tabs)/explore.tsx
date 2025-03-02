0876b2ad65b77d97f0bd83897cacd36c3abfc725
esionascimento <esionascimento@proton.me>
2025-03-02 12:58:14 -0400

    refactor: explore page move codigo components

--- src/app/(tabs)/explore.tsx
+++ src/app/(tabs)/explore.tsx
@@ -1,129 +1,82 @@
-import { StyleSheet, Image, Platform } from 'react-native'
+import React, { useState } from 'react'
+import { SafeAreaView, Text, StyleSheet, StatusBar } from 'react-native'
 
-import { Collapsible } from '@/src/components/Collapsible'
-import { ExternalLink } from '@/src/components/ExternalLink'
-import ParallaxScrollView from '@/src/components/ParallaxScrollView'
-import { ThemedText } from '@/src/components/ThemedText'
-import { ThemedView } from '@/src/components/ThemedView'
-import { IconSymbol } from '@/src/components/ui/IconSymbol'
+import CoordenadasForm from '@/src/components/CoordenadasForm'
+import CoordenadasList from '@/src/components/CoordenadasList'
+import { Coordenada } from '@/src/models/Coordenada'
 
 export default function TabTwoScreen() {
+  const [coordenadas, setCoordenadas] = useState<Coordenada[]>([
+    {
+      nome: 'CCB Tancredo Neves',
+      latitude: -8.760491,
+      longitude: -63.835211,
+      atualizacao: '26/02/2025',
+    },
+    {
+      nome: 'CCB Cascalheira(Flamboyant)',
+      latitude: -8.778104,
+      longitude: -63.837291,
+      atualizacao: '26/02/2025',
+    },
+    {
+      nome: 'CCB Paraiso das Acácias',
+      latitude: -8.77895,
+      longitude: -63.596662,
+      atualizacao: '26/02/2025',
+    },
+    {
+      nome: 'CCB Estrada da Aliança',
+      latitude: -8.642954,
+      longitude: -63.611601,
+      atualizacao: '26/02/2025',
+    },
+    {
+      nome: 'CCB Ramal Bom Jesus',
+      latitude: -8.831721,
+      longitude: -63.781003,
+      atualizacao: '26/02/2025',
+    },
+    {
+      nome: 'CCB KM 13',
+      latitude: -8.805448,
+      longitude: -63.777817,
+      atualizacao: '26/02/2025',
+    },
+    {
+      nome: 'CCB Candeias Central',
+      latitude: -8.795603,
+      longitude: -63.700311,
+      atualizacao: '26/02/2025',
+    },
+    {
+      nome: 'CCB Candeias Santa Letícia',
+      latitude: -8.787536,
+      longitude: -63.690464,
+      atualizacao: '26/02/2025',
+    },
+  ])
+
+  const adicionarCoordenada = (coordenada: Coordenada) => {
+    setCoordenadas([...coordenadas, coordenada])
+  }
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-          style={styles.headerImage}
-        />
-      }
-    >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Casa de Oração</ThemedText>
-      </ThemedView>
-      <ThemedText>
-        This app includes example code to help you get started.
-      </ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{' '}
-          and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the
-          web version, press <ThemedText type="defaultSemiBold">w</ThemedText>{' '}
-          in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the{' '}
-          <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to
-          provide files for different screen densities
-        </ThemedText>
-        <Image
-          source={require('@/assets/images/react-logo.png')}
-          style={{ alignSelf: 'center' }}
-        />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Custom fonts">
-        <ThemedText>
-          Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText>{' '}
-          to see how to load{' '}
-          <ThemedText style={{ fontFamily: 'SpaceMono' }}>
-            custom fonts such as this one.
-          </ThemedText>
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{' '}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook
-          lets you inspect what the user's current color scheme is, and so you
-          can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{' '}
-          <ThemedText type="defaultSemiBold">
-            components/HelloWave.tsx
-          </ThemedText>{' '}
-          component uses the powerful{' '}
-          <ThemedText type="defaultSemiBold">
-            react-native-reanimated
-          </ThemedText>{' '}
-          library to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The{' '}
-              <ThemedText type="defaultSemiBold">
-                components/ParallaxScrollView.tsx
-              </ThemedText>{' '}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
-    </ParallaxScrollView>
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="dark-content" />
+      <Text style={styles.title}>Lista de Coordenadas</Text>
+      <CoordenadasForm onAdicionar={adicionarCoordenada} />
+      <CoordenadasList coordenadas={coordenadas} />
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    gap: 8,
+  container: { flex: 1, padding: 20 },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 10,
   },
 })
