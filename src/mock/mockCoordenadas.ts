c219d7b8194baa192c77d30de84b07517f33001a
esionascimento <esionascimento@proton.me>
2025-03-03 12:21:36 -0400

    feat: mockCoordenadas

---
+++ src/mock/mockCoordenadas.ts
@@ -0,0 +1,108 @@
+import { Coordenada } from '../models/Coordenada'
+
+export const mockCoordenadas: Coordenada[] = [
+  {
+    id: '1',
+    nome: 'CCB Tancredo Neves',
+    latitude: -8.760491,
+    longitude: -63.835211,
+    endereco: {
+      cep: 76829560,
+      cidade: 'Porto Velho',
+      logradouro: 'Rua Antônio Violão',
+      numero: 3675,
+    },
+    atualizacao: '26/02/2025',
+  },
+  {
+    id: '2',
+    nome: 'CCB Cascalheira(Flamboyant)',
+    latitude: -8.778104,
+    longitude: -63.837291,
+    endereco: {
+      cep: 1321,
+      cidade: '',
+      logradouro: '',
+      numero: 12321,
+    },
+    atualizacao: '26/02/2025',
+  },
+  {
+    id: '3',
+    nome: 'CCB Paraiso das Acácias',
+    latitude: -8.77895,
+    longitude: -63.596662,
+    endereco: {
+      cep: 1321,
+      cidade: '',
+      logradouro: '',
+      numero: 12321,
+    },
+    atualizacao: '26/02/2025',
+  },
+  {
+    id: '4',
+    nome: 'CCB Estrada da Aliança',
+    latitude: -8.642954,
+    longitude: -63.611601,
+    endereco: {
+      cep: 1321,
+      cidade: '',
+      logradouro: '',
+      numero: 12321,
+    },
+    atualizacao: '26/02/2025',
+  },
+  {
+    id: '5',
+    nome: 'CCB Ramal Bom Jesus',
+    latitude: -8.831721,
+    longitude: -63.781003,
+    endereco: {
+      cep: 1321,
+      cidade: '',
+      logradouro: '',
+      numero: 12321,
+    },
+    atualizacao: '26/02/2025',
+  },
+  {
+    id: '6',
+    nome: 'CCB KM 13',
+    latitude: -8.805448,
+    longitude: -63.777817,
+    endereco: {
+      cep: 1321,
+      cidade: '',
+      logradouro: '',
+      numero: 12321,
+    },
+    atualizacao: '26/02/2025',
+  },
+  {
+    id: '7',
+    nome: 'CCB Candeias Central',
+    latitude: -8.795603,
+    longitude: -63.700311,
+    endereco: {
+      cep: 1321,
+      cidade: '',
+      logradouro: '',
+      numero: 12321,
+    },
+    atualizacao: '26/02/2025',
+  },
+  {
+    id: '8',
+    nome: 'CCB Candeias Santa Letícia',
+    latitude: -8.787536,
+    longitude: -63.690464,
+    endereco: {
+      cep: 1321,
+      cidade: '',
+      logradouro: '',
+      numero: 12321,
+    },
+    atualizacao: '26/02/2025',
+  },
+]
