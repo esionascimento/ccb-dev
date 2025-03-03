24517030c0516cc5102f9de33ed6ad3514890e81
esionascimento <esionascimento@proton.me>
2025-03-03 12:32:16 -0400

    refactor: Coordenada models add props

--- src/models/Coordenada.ts
+++ src/models/Coordenada.ts
@@ -1,6 +1,30 @@
 export interface Coordenada {
+  id: string
   nome: string
   latitude: number
   longitude: number
   atualizacao: string
+  endereco: Endereco
+  diasCulto?: {
+    cultos: string
+    reuniaoJovemMenores: string
+  }
+  ministerio?: {
+    cooperadorOficioMinisterial: string
+    diacono: string
+    cooperadorJovensMenores: string
+  }
+  infoAdicionais?: {
+    adm?: string
+    ra?: string
+    rrm?: string
+    atr?: string
+  }
+}
+
+interface Endereco {
+  logradouro: string
+  cep: number
+  cidade: string
+  numero: number
 }
