4dc088574ec61fb48ecdf6854bb8cade1dc0e2a0
esionascimento <esionascimento@proton.me>
2025-03-03 12:36:18 -0400

    refactor: _layout import SafeAreProvider, SafeAreaView

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,50 +1,59 @@
 import { Tabs } from 'expo-router'
 import React from 'react'
 import { Platform } from 'react-native'
+import { StyleSheet } from 'react-native'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 
 import { HapticTab } from '@/src/components/HapticTab'
 import { IconSymbol } from '@/src/components/ui/IconSymbol'
 import TabBarBackground from '@/src/components/ui/TabBarBackground'
-import { Colors } from '@/src/constants/Colors'
 import { useColorScheme } from '@/src/hooks/useColorScheme'
 
 export default function TabLayout() {
   const colorScheme = useColorScheme()
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Início',
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="house.fill" color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Casa de Oração',
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="paperplane.fill" color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+    <>
+      <SafeAreaProvider>
+        <SafeAreaView style={styles.safeArea}>
+          <Tabs
+            screenOptions={{
+              headerShown: false,
+              tabBarButton: HapticTab,
+              tabBarBackground: TabBarBackground,
+              tabBarStyle: Platform.select({
+                ios: {
+                  position: 'absolute',
+                },
+                default: {},
+              }),
+            }}
+          >
+            <Tabs.Screen
+              name="index"
+              options={{
+                title: 'Início',
+                tabBarIcon: ({ color }) => (
+                  <IconSymbol size={28} name="house.fill" color={color} />
+                ),
+              }}
+            />
+            <Tabs.Screen
+              name="explore"
+              options={{
+                title: 'Casa de Oração',
+                tabBarIcon: ({ color }) => (
+                  <IconSymbol size={28} name="paperplane.fill" color={color} />
+                ),
+              }}
+            />
+          </Tabs>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </>
   )
 }
+
+const styles = StyleSheet.create({
+  safeArea: { flex: 1 },
+})
