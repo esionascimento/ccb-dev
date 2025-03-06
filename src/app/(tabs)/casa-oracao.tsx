5f3b1df6b5cea5a07e9605f1f8012253d8b4440c
esionascimento <esionascimento@proton.me>
2025-03-06 01:26:17 -0400

    refactor: casa-oracao app import Menu, Modal

--- src/app/(tabs)/casa-oracao.tsx
+++ src/app/(tabs)/casa-oracao.tsx
@@ -5,32 +5,55 @@ import { Text } from 'react-native-paper'
 import CoordenadasForm from '@/src/components/CoordenadasForm'
 import CoordenadasList from '@/src/components/CoordenadasList'
 import { Coordenada } from '@/src/models/Coordenada'
+import { HeaderMenuCasaOracao } from '@/src/sections/casaOracao/components/HeaderMenuCasaOracao'
+import { ModalFiltroCasaOracao } from '@/src/sections/casaOracao/components/ModalFiltro'
 import { mockCoordenadas } from '../../mock/mockCoordenadas'
 
-export default function TabTwoScreen() {
+export default function TabCasaOracao() {
   const [coordenadas, setCoordenadas] = useState<Coordenada[]>(mockCoordenadas)
+  const [isMenu, setMenuVisible] = useState(false)
+  const [modalVisible, setModalVisible] = useState(false)
 
   const adicionarCoordenada = (coordenada: Coordenada) => {
     setCoordenadas([...coordenadas, coordenada])
+  }
+
+  const handleResetCoordenadas = () => {
+    setCoordenadas(mockCoordenadas)
   }
 
   return (
     <>
       <View style={styles.container}>
         <Text style={styles.title}>Lista de Coordenadas</Text>
-        <CoordenadasForm onAdicionar={adicionarCoordenada} />
+        <HeaderMenuCasaOracao
+          setModalVisible={setModalVisible}
+          isMenu={isMenu}
+          setMenu={setMenuVisible}
+          handleResetCoordenadas={handleResetCoordenadas}
+        />
+
+        {/* <CoordenadasForm onAdicionar={adicionarCoordenada} /> */}
         <CoordenadasList coordenadas={coordenadas} />
       </View>
+
+      <ModalFiltroCasaOracao
+        open={modalVisible}
+        setModalVisible={setModalVisible}
+        coordenadas={coordenadas}
+        setCoordenadas={setCoordenadas}
+      />
     </>
   )
 }
 
 const styles = StyleSheet.create({
+  safeArea: { flex: 1 },
   container: { flex: 1, padding: 8 },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
-    textAlign: 'center',
     marginBottom: 10,
+    marginLeft: 8,
   },
 })
