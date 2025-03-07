bda746f04abb4b5fe38ebae89052c2cfb6a884a1
esionascimento <esionascimento@proton.me>
2025-03-07 18:55:43 -0400

    feat: Localizacao section coordenadas

---
+++ src/sections/coordenadaDetalhes/components/Localizacao.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+import { Card, Text } from 'react-native-paper'
+
+import { InfoText } from '@/src/sections/casaOracao/components/InfoText'
+import { Coordenada } from '@/src/models/Coordenada'
+
+type Props = {
+  coordenada: Coordenada
+}
+
+export function CoordenadaLocalizacao({ coordenada }: Props) {
+  return (
+    <View>
+      <Card style={{ elevation: 4, borderRadius: 8, padding: 10 }}>
+        <Text style={styles.titulo}>Localização</Text>
+        <View>
+          <InfoText
+            label="Logradouro"
+            value={coordenada.endereco?.logradouro}
+          />
+          <InfoText label="CEP" value={coordenada.endereco?.cep} />
+          <InfoText label="Cidade" value={coordenada.endereco?.cidade} />
+          <InfoText label="Latitude" value={coordenada.latitude} />
+          <InfoText label="Longitude" value={coordenada.longitude} />
+        </View>
+      </Card>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  titulo: { fontSize: 22, fontWeight: 'bold', marginBottom: 8 },
+})
