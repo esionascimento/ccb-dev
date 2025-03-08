f8be743b6d2fb3e6f54bfd26b3a2d3c0c21c2265
esionascimento <esionascimento@proton.me>
2025-03-08 01:09:58 -0400

    delete: mapsLauncher

--- src/utils/mapsLauncher.ts
+++
@@ -1,9 +0,0 @@
-import { Linking, Alert } from 'react-native'
-
-export const abrirMapa = (latitude: number, longitude: number) => {
-  const url = `https://www.google.com/maps/dir/?api=1&destination=${latitude},${longitude}`
-
-  Linking.openURL(url).catch(() =>
-    Alert.alert('Erro', 'Nenhum app de mapas encontrado!'),
-  )
-}
