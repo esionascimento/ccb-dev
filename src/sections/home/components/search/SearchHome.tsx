600ed6ef2c9fb50a7dbd4a45edb984b9b9cdaec1
esionascimento <esionascimento@proton.me>
2025-03-13 17:58:02 -0400

    feat: SearchHome components home

---
+++ src/sections/home/components/search/SearchHome.tsx
@@ -0,0 +1,71 @@
+import { StyleSheet, TouchableOpacity, View } from 'react-native'
+import AntDesign from '@expo/vector-icons/AntDesign'
+import Fontisto from '@expo/vector-icons/Fontisto'
+import { Text, useTheme } from 'react-native-paper'
+
+import { useIsDark } from '@/src/hooks/useIsDark'
+import { router } from 'expo-router'
+
+type Props = {
+  setOpcao: any
+}
+
+export function SearchHome({ setOpcao }: Props) {
+  const theme = useTheme()
+  const isDark = useIsDark()
+
+  return (
+    <View>
+      <View style={styles.gridContainer}>
+        <TouchableOpacity
+          onPress={() => router.push(`/busca-rapida`)}
+          style={{
+            ...styles.button,
+            backgroundColor: theme.colors.buttonBackground,
+            borderColor: theme.colors.secondary,
+          }}
+        >
+          <AntDesign name="search1" size={30} color={isDark ? 'white' : 'black'} />
+          <Text style={styles.buttonText}>Procura Rápida</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          disabled
+          style={{
+            ...styles.button,
+            backgroundColor: theme.colors.buttonBackground,
+            borderColor: theme.colors.secondary,
+          }}
+          onPress={() => setOpcao('Busca Geográfica')}
+        >
+          <Fontisto name="world-o" size={30} color={isDark ? 'white' : 'black'} />
+          <Text style={styles.buttonText}>Busca Geográfica</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  gridContainer: {
+    flexDirection: 'row',
+  },
+  button: {
+    flex: 1,
+    backgroundColor: 'silver',
+    padding: 10,
+    margin: 3,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 8,
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+  },
+  buttonText: {
+    marginTop: 3,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+})
