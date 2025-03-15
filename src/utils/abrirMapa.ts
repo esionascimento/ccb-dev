a7a85844f67221c0dc2cc79751be8f70bc604619
esionascimento <esionascimento@proton.me>
2025-03-15 03:15:12 -0400

    refactor: abrirMapa para android e apple

--- src/utils/abrirMapa.ts
+++ src/utils/abrirMapa.ts
@@ -1,12 +1,41 @@
-import { Alert, Linking } from 'react-native'
+import { Linking, Alert, Platform } from 'react-native'
 
-export const abrirMapa = (latitude: number, longitude: number) => {
-  const googleMapsURL = `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`
-  const appleMapsURL = `maps://?q=${latitude},${longitude}`
+export const abrirMapa = async (options: { latitude?: number; longitude?: number } | { endereco: string }) => {
+  let googleMapsURL = ''
+  let appleMapsURL = ''
 
-  Linking.openURL(appleMapsURL).catch(() =>
-    Linking.openURL(googleMapsURL).catch(() =>
-      Alert.alert('Erro', 'Nenhum app de mapas encontrado!'),
-    ),
-  )
+  if ('latitude' in options && 'longitude' in options) {
+    const { latitude, longitude } = options
+    googleMapsURL = `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`
+    appleMapsURL = `maps://?q=${latitude},${longitude}`
+  } else if ('endereco' in options) {
+    const { endereco } = options
+    googleMapsURL = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(endereco)}`
+    appleMapsURL = `maps://?q=${encodeURIComponent(endereco)}`
+  } else {
+    Alert.alert('Erro', 'Nenhuma informação de localização fornecida!')
+    return
+  }
+
+  const canOpenAppleMaps = await Linking.canOpenURL(appleMapsURL)
+  const canOpenGoogleMaps = await Linking.canOpenURL(googleMapsURL)
+
+  if (canOpenAppleMaps && canOpenGoogleMaps) {
+    Alert.alert(
+      'Escolha o mapa',
+      'Qual app de mapas você deseja usar?',
+      [
+        { text: 'Apple Maps', onPress: () => Linking.openURL(appleMapsURL) },
+        { text: 'Google Maps', onPress: () => Linking.openURL(googleMapsURL) },
+        { text: 'Cancelar', style: 'cancel' },
+      ],
+      { cancelable: true },
+    )
+  } else if (canOpenAppleMaps) {
+    Linking.openURL(appleMapsURL)
+  } else if (canOpenGoogleMaps) {
+    Linking.openURL(googleMapsURL)
+  } else {
+    Alert.alert('Erro', 'Nenhum app de mapas encontrado!')
+  }
 }
