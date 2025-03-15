b55e88cfb2d1fdfc55c5ee9a4f191d59aef8915b
esionascimento <esionascimento@proton.me>
2025-03-15 02:26:58 -0400

    refactor: casaOracaoSection add quantidade de casas, inicializar vazio

--- src/sections/casaOracao/CasaOracaoSection.tsx
+++ src/sections/casaOracao/CasaOracaoSection.tsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react'
-import { StyleSheet, View } from 'react-native'
 
 import CoordenadasList from '@/src/sections/casaOracao/components/CoordenadasList'
 import { Coordenada } from '@/src/models/Coordenada'
 import { HeaderMenuCasaOracao } from '@/src/sections/casaOracao/components/HeaderMenuCasaOracao'
 import { ModalFiltroCasaOracaoSection } from '@/src/sections/casaOracao/components/ModalFiltro'
 import { CoordenadasSearch } from '@/src/sections/casaOracao/components/CoordenadasSearch'
+import { ThemedText } from '@/src/components/ThemedText'
 import { mockCoordenadas } from '../../mock/mockCoordenadas'
 
 export function TabCasaOracaoSection() {
-  const [coordenadasSearch, setCoordenadasSearch] = useState<Coordenada[]>(mockCoordenadas)
+  const [coordenadasSearch, setCoordenadasSearch] = useState<Coordenada[]>([])
   const [coordenadas, setCoordenadas] = useState<Coordenada[]>([])
   const [isMenu, setMenuVisible] = useState(false)
   const [modalVisible, setModalVisible] = useState(false)
@@ -24,17 +24,16 @@ export function TabCasaOracaoSection() {
 
   return (
     <>
-      <View>
-        <HeaderMenuCasaOracao
-          setModalVisible={setModalVisible}
-          isMenu={isMenu}
-          setMenu={setMenuVisible}
-          handleResetCoordenadas={handleResetCoordenadas}
-        />
+      <HeaderMenuCasaOracao
+        setModalVisible={setModalVisible}
+        isMenu={isMenu}
+        setMenu={setMenuVisible}
+        handleResetCoordenadas={handleResetCoordenadas}
+      />
 
-        <CoordenadasSearch setCoordenadas={setCoordenadas} coordenadasSearch={coordenadasSearch} />
-        <CoordenadasList coordenadas={coordenadas} />
-      </View>
+      <CoordenadasSearch setCoordenadas={setCoordenadas} coordenadasSearch={coordenadasSearch} />
+      <ThemedText>{coordenadas?.length} casas de orações</ThemedText>
+      <CoordenadasList coordenadas={coordenadas} />
 
       <ModalFiltroCasaOracaoSection
         open={modalVisible}
