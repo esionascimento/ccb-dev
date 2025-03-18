5a9d80b894a69abcd69b13256d3275dd9db7ca90
esionascimento <esionascimento@proton.me>
2025-03-18 12:18:40 -0400

    feat: BuscaRapidaSearch

---
+++ src/sections/buscaRapida/components/BuscaRapidaSearch.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+import { TextInput } from 'react-native-paper'
+
+import { serviceCoordenada } from '@/src/services/coordenada/coordenada.service'
+import { Coordenada } from '../../../models/Coordenada'
+
+interface Props {
+  setCoordenadas: React.Dispatch<React.SetStateAction<Coordenada[]>>
+}
+
+export const BuscaRapidaSearch = ({ setCoordenadas }: Props) => {
+  const changeSeach = async (nome: string) => {
+    if (nome?.length > 2) {
+      const response = await serviceCoordenada.search({
+        nome,
+      })
+
+      setCoordenadas(response)
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <TextInput label={'Nome do Local'} style={styles.input} onChangeText={changeSeach} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: { marginBottom: 8 },
+  input: {
+    marginVertical: 5,
+    borderRadius: 5,
+  },
+})
