d94a577473a161883a054dc2f8788175c5068a6e
esionascimento <esionascimento@proton.me>
2025-03-18 12:17:43 -0400

    refactor: index move code section

--- src/app/busca-rapida/index.tsx
+++ src/app/busca-rapida/index.tsx
@@ -1,8 +1,7 @@
-import { StyleSheet, View } from 'react-native'
-
 import { useLayoutEffect } from 'react'
 import { useNavigation } from 'expo-router'
-import { TextInput } from 'react-native-paper'
+
+import { BuscaRapidaSection } from '@/src/sections/buscaRapida/BuscaRapidaSection'
 
 export default function BuscaRapidaScreen() {
   const navigation = useNavigation()
@@ -16,33 +15,7 @@ export default function BuscaRapidaScreen() {
 
   return (
     <>
-      <View style={styles.container}>
-        <TextInput label={'Nome do Local'} style={styles.input} />
-      </View>
+      <BuscaRapidaSection />
     </>
   )
 }
-
-const styles = StyleSheet.create({
-  container: { marginBottom: 8 },
-  input: {
-    marginVertical: 5,
-    borderRadius: 5,
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-})
