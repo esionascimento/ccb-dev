65e136b60eb69e9be2de378bc1b1ee9b45ef7041
esionascimento <esionascimento@proton.me>
2025-03-19 10:30:29 -0400

    refactor: hino.service add prop coro

--- src/services/hino/hino.service.ts
+++ src/services/hino/hino.service.ts
@@ -2,18 +2,18 @@ import { dataHinos } from '@/src/api/dataHinos'
 import { Hino } from '@/src/models/Hinos'
 
 export const serviceHino = {
-  search: async ({ hino }: { hino?: number }): Promise<Hino> => {
+  search: async ({ hino, coro = false }: { hino: number; coro?: boolean }): Promise<Hino> => {
     return new Promise((resolve) => {
       setTimeout(() => {
         let search
 
         if (hino) {
-          search = dataHinos.find((item) => item?.hino === hino && !item?.coro)
+          search = dataHinos.find((item) => item?.hino === hino && item?.coro === coro)
           if (search) {
             resolve(search)
           }
         }
-      }, 100)
+      }, 10)
     })
   },
 }
