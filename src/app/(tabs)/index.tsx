add25af52ebc6f27c597ad05ff7aeb480122e2fc
esionascimento <esionascimento@proton.me>
2025-03-24 15:49:29 -0400

    refactor: index tabs remove ParallaxScrollView add LayoutHeader

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,31 +1,32 @@
-import { Image, StyleSheet, Platform } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 
 import { HelloWave } from '@/src/components/HelloWave'
-import ParallaxScrollView from '@/src/components/ParallaxScrollView'
 import { ThemedText } from '@/src/components/ThemedText'
 import { ThemedView } from '@/src/components/ThemedView'
 import { HomeSection } from '@/src/sections/home/HomeSection'
+import { LayoutHeader } from '@/src/layouts/header/layout-header'
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={<Image source={require('@/assets/images/partial-react-logo.png')} style={styles.reactLogo} />}
-    >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">CCB!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <HomeSection />
-    </ParallaxScrollView>
+    <LayoutHeader>
+      <View style={styles.container}>
+        <ThemedView style={styles.titleContainer}>
+          <ThemedText type="title">CCB!</ThemedText>
+          <HelloWave />
+        </ThemedView>
+        <HomeSection />
+      </View>
+    </LayoutHeader>
   )
 }
 
 const styles = StyleSheet.create({
+  container: { padding: 8 },
   titleContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     gap: 8,
+    marginTop: 3,
   },
   stepContainer: {
     gap: 8,
