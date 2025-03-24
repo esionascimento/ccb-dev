b5cf270d22589aa8fd18c3c94194f210860547cc
esionascimento <esionascimento@proton.me>
2025-03-24 15:53:15 -0400

    refactor: metro.config.js change mp3->wav

--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,6 @@ const { getDefaultConfig } = require('expo/metro-config')
 
 module.exports = (() => {
   const config = getDefaultConfig(__dirname)
-  config.resolver.assetExts.push('mp3')
+  config.resolver.assetExts.push('wav')
   return config
 })()
