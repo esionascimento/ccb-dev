a4b7c4a916eab110bd8df7e03ceff5b3aae4eb20
esionascimento <esionascimento@proton.me>
2025-03-24 15:47:51 -0400

    feat: contants PATH

---
+++ src/constants/routes.ts
@@ -0,0 +1,7 @@
+import { RelativePathString } from 'expo-router'
+
+export const PATH = {
+  settings: '/settings' as RelativePathString,
+  buscaRapida: '/busca-rapida' as RelativePathString,
+  metronomo: '/metronomo' as RelativePathString,
+}
