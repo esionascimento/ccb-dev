0ee5e2d2cde6482eabdc32259b93797b9fa84119
esionascimento <esionascimento@proton.me>
2025-03-25 00:02:32 -0400

    refactor: storageService add tipagem, e save alterado, como um metodo PUT

--- src/services/storageService.ts
+++ src/services/storageService.ts
@@ -1,22 +1,30 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { Toast } from 'toastify-react-native'
 import { EventEmitter } from 'events'
+import { StorageKeys, StorageValues } from './storage.interface'
 
 const storageEventEmitter = new EventEmitter()
+
 export const storageService = {
-  save: async (key: string, value: any) => {
+  save: async <K extends StorageKeys>(key: K, value: Partial<StorageValues[K]>) => {
     try {
-      const jsonValue = JSON.stringify(value)
+      const existingData = await AsyncStorage.getItem(key)
+      const parsedData: StorageValues[K] = existingData ? JSON.parse(existingData) : {}
+
+      const updatedData = { ...parsedData, ...value }
+
+      const jsonValue = JSON.stringify(updatedData)
       await AsyncStorage.setItem(key, jsonValue)
-      storageEventEmitter.emit(key, value) // Notifica mudança
-      Toast.success('Sucesso salvar dados')
+
+      storageEventEmitter.emit(key, updatedData)
+      Toast.success('Dados atualizados com sucesso')
     } catch (error) {
       console.error('Erro ao salvar dados:', error)
       Toast.error('Erro ao salvar dados')
     }
   },
 
-  get: async (key: string) => {
+  get: async <K extends StorageKeys>(key: K): Promise<StorageValues[K] | null> => {
     try {
       const jsonValue = await AsyncStorage.getItem(key)
       return jsonValue ? JSON.parse(jsonValue) : null
