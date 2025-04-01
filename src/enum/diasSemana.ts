bab367fed978b532e080a86bc91265d94348aa6e
esionascimento <esionascimento@proton.me>
2025-04-01 00:47:19 -0400

    chore: diasSemana create diasSemana

--- src/enum/diasSemana.ts
+++ src/enum/diasSemana.ts
@@ -1,4 +1,4 @@
-export const DiasSemana: Record<string, string> = {
+export const DiasSemanaLabel: Record<string, string> = {
   domingo: 'Domingo',
   segunda: 'Segunda-feira',
   terca: 'Terça-feira',
@@ -7,3 +7,13 @@ export const DiasSemana: Record<string, string> = {
   sexta: 'Sexta-feira',
   sabado: 'Sábado',
 }
+
+export const diasSemana = [
+  { id: 'dom', label: 'Domingo', numero: 1 },
+  { id: 'seg', label: 'Segunda-feira', numero: 2 },
+  { id: 'ter', label: 'Terça-feira', numero: 3 },
+  { id: 'qua', label: 'Quarta-feira', numero: 4 },
+  { id: 'qui', label: 'Quinta-feira', numero: 5 },
+  { id: 'sex', label: 'Sexta-feira', numero: 6 },
+  { id: 'sab', label: 'Sábado', numero: 7 },
+]
